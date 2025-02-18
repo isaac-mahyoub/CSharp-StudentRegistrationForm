f50653f23bfcd03c7abe06229ec364b6fb820749
isaac-mahyoub <eng.isaacmahyoub@gmail.com>
2025-02-18 19:44:40 +0300

    Initial version: it can be developed in the future.

---
+++ RegistrationStudentForm/RegistrationStudentForm/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace RegistrationStudentForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbOther = new System.Windows.Forms.RadioButton();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.lblBirthdate = new System.Windows.Forms.Label();
+            this.dtpBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.txtName.Location = new System.Drawing.Point(294, 49);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(522, 38);
+            this.txtName.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.txtEmail.Location = new System.Drawing.Point(294, 116);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(522, 38);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.txtPassword.Location = new System.Drawing.Point(294, 190);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(522, 38);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(294, 265);
+            this.rbMale.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(66, 25);
+            this.rbMale.TabIndex = 4;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(522, 265);
+            this.rbFemale.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(85, 25);
+            this.rbFemale.TabIndex = 5;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbOther
+            // 
+            this.rbOther.AutoSize = true;
+            this.rbOther.Location = new System.Drawing.Point(735, 265);
+            this.rbOther.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbOther.Name = "rbOther";
+            this.rbOther.Size = new System.Drawing.Size(73, 25);
+            this.rbOther.TabIndex = 6;
+            this.rbOther.TabStop = true;
+            this.rbOther.Text = "Other";
+            this.rbOther.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblName.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.lblName.Location = new System.Drawing.Point(172, 49);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(92, 38);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Name";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEmail.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.lblEmail.Location = new System.Drawing.Point(172, 116);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(92, 38);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Email";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPassword.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.lblPassword.Location = new System.Drawing.Point(168, 190);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(96, 38);
+            this.lblPassword.TabIndex = 9;
+            this.lblPassword.Text = "Password";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGender
+            // 
+            this.lblGender.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblGender.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.lblGender.Location = new System.Drawing.Point(168, 258);
+            this.lblGender.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(92, 38);
+            this.lblGender.TabIndex = 10;
+            this.lblGender.Text = "Gender";
+            this.lblGender.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Location = new System.Drawing.Point(294, 413);
+            this.cmbCountry.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(154, 29);
+            this.cmbCountry.TabIndex = 13;
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCountry.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.lblCountry.Location = new System.Drawing.Point(168, 407);
+            this.lblCountry.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(92, 38);
+            this.lblCountry.TabIndex = 14;
+            this.lblCountry.Text = "Country";
+            this.lblCountry.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.btnSubmit.Location = new System.Drawing.Point(720, 413);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(96, 39);
+            this.btnSubmit.TabIndex = 15;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.lblResult.Location = new System.Drawing.Point(168, 480);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(66, 23);
+            this.lblResult.TabIndex = 16;
+            this.lblResult.Text = "Result";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.btnColor.Location = new System.Drawing.Point(682, 343);
+            this.btnColor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(135, 39);
+            this.btnColor.TabIndex = 17;
+            this.btnColor.Text = "Favorit Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            // 
+            // lblBirthdate
+            // 
+            this.lblBirthdate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBirthdate.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.lblBirthdate.Location = new System.Drawing.Point(168, 336);
+            this.lblBirthdate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBirthdate.Name = "lblBirthdate";
+            this.lblBirthdate.Size = new System.Drawing.Size(92, 38);
+            this.lblBirthdate.TabIndex = 18;
+            this.lblBirthdate.Text = "Birthdate";
+            this.lblBirthdate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dtpBirthdate
+            // 
+            this.dtpBirthdate.Location = new System.Drawing.Point(294, 346);
+            this.dtpBirthdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpBirthdate.Name = "dtpBirthdate";
+            this.dtpBirthdate.Size = new System.Drawing.Size(256, 28);
+            this.dtpBirthdate.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1029, 591);
+            this.Controls.Add(this.dtpBirthdate);
+            this.Controls.Add(this.lblBirthdate);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.rbOther);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Student Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbOther;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Label lblBirthdate;
+        private System.Windows.Forms.DateTimePicker dtpBirthdate;
+
+
+    }
+}
+
---
+++ RegistrationStudentForm/RegistrationStudentForm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegistrationStudentForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnSubmit.Click += btnSubmit_Click;
+            btnColor.Click += btnColor_Click;
+            this.Load += Form1_Load;
+
+            rbMale.Appearance = Appearance.Button;
+            rbMale.FlatStyle = FlatStyle.Standard;
+            rbMale.BackColor = Color.White;
+            rbMale.FlatAppearance.BorderColor = Color.Blue;
+
+            rbFemale.Appearance = Appearance.Button;
+            rbFemale.FlatStyle = FlatStyle.Standard;
+            rbFemale.BackColor = Color.White;
+            rbFemale.FlatAppearance.BorderColor = Color.Blue;
+
+            rbOther.Appearance = Appearance.Button;
+            rbOther.FlatStyle = FlatStyle.Standard;
+            rbOther.BackColor = Color.White;
+            rbOther.FlatAppearance.BorderColor = Color.Blue;
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCountry.Items.AddRange(new string[] { "Yemen", "Egypt", "Iraq", "Saudi Arabia", "Palestine", "Syria" });
+
+            this.BackColor = Color.FromArgb(240, 250, 255);
+
+            btnSubmit.BackColor = Color.FromArgb(0, 122, 255);
+            btnSubmit.ForeColor = Color.White;
+            btnColor.BackColor = Color.FromArgb(242, 242, 242);
+            lblResult.ForeColor = Color.Black;
+            txtName.ForeColor = Color.Black;
+            txtEmail.ForeColor = Color.Black;
+            txtPassword.ForeColor = Color.Black;
+            lblResult.ForeColor = Color.WhiteSmoke;
+
+
+            lblName.BackColor = Color.FromArgb(80, 200, 240,255);
+            lblEmail.BackColor = Color.FromArgb(80, 200, 240,255);
+            lblPassword.BackColor = Color.FromArgb(80, 200, 240,255);
+            lblBirthdate.BackColor = Color.FromArgb(80, 200, 240,255);
+            lblCountry.BackColor = Color.FromArgb(80, 200, 240,255);
+            lblGender.BackColor = Color.FromArgb(80, 200, 240,255);
+
+
+            txtName.BackColor = Color.FromArgb(255, 255, 255);
+            txtName.BorderStyle = BorderStyle.Fixed3D;
+            txtEmail.BackColor = Color.FromArgb(255, 255, 255);
+            txtEmail.BorderStyle = BorderStyle.Fixed3D;
+            txtPassword.BackColor = Color.FromArgb(255, 255, 255);
+            txtPassword.BorderStyle = BorderStyle.Fixed3D;
+            lblResult.BackColor = Color.Green;
+
+        }
+        
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Check the name field
+                if (string.IsNullOrWhiteSpace(txtName.Text))
+                {
+                    MessageBox.Show("Please enter your name!", "Required Field", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtName.Focus();
+                    return;
+                }
+                // Check the email field
+                if (string.IsNullOrWhiteSpace(txtEmail.Text))
+                {
+                    MessageBox.Show("Please enter your email!", "Required Field", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtEmail.Focus();
+                    return;
+                }
+                // Verify email format
+                var emailRegex = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$");
+                if (!emailRegex.IsMatch(txtEmail.Text))
+                {
+                    MessageBox.Show("Invalid email!", "Required Field", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtEmail.Focus();
+                    return;
+                }
+                // Check the password field
+                if (string.IsNullOrWhiteSpace(txtPassword.Text))
+                {
+                    MessageBox.Show("Please enter your password!", "Required Field", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtPassword.Focus();
+                    return;
+                }
+
+                string gender = "";
+                if (rbMale.Checked) gender = "Male";
+                else if (rbFemale.Checked) gender = "Female";
+                else if (rbOther.Checked) gender = "Other";
+
+                lblResult.Text = $"Name: {txtName.Text}, " +
+                                 $"Email: {txtEmail.Text}, " +
+                                 $"Gender: {gender},\n" +
+                                 $"Fivorite Color: {btnColor.BackColor.Name}, " +
+                                 $"Birthdate: {dtpBirthdate.Value.ToShortDateString()}, " +
+                                 $"Country: {cmbCountry.SelectedItem.ToString()}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // When you click on the color selection button
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            // Open the color selsction window
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Set the selscted color in the button
+                    btnColor.BackColor = colorDialog.Color;
+                }
+            }
+        }
+
+
+    }
+}
